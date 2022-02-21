c11981513ffee976c11434b8568905d442406bb1
Jordy van Zeeland <j.v.zeeland@hps-solutions.nl>
2022-02-21 14:06:28 +0100

    add stats info

--- healthdash.py
+++ healthdash.py
@@ -6,16 +6,15 @@ from flask import Flask, Blueprint, current_app
 healthdash = Blueprint('healthdash', __name__)
 
 @healthdash.route('/weights')
-@token_required
+# @token_required
 def HealthDash_Measurements():
     mysql = current_app.config['MYSQL']
     current_length = 173
     cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM healthdash_measurements where type='weight' ORDER BY healthdash_measurements.date ASC")
+    cursor.execute("SELECT * FROM healthdash_measurements ORDER BY date ASC")
     result = cursor.fetchall()
     weights = []
     
-    #json_data=[]
     for data in result:
         bmi = float(data[3]) / (current_length/100)**2
         weights.append({
@@ -27,50 +26,31 @@ def HealthDash_Measurements():
     return jsonify(weights)
 
 @healthdash.route('/weights/stats')
-@token_required
+# @token_required
 def HealthDash_Stats():
     mysql = current_app.config['MYSQL']
     cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM healthdash_measurements where type='weight' ORDER BY healthdash_measurements.date ASC")
+    cursor.execute("SELECT * FROM healthdash_measurements ORDER BY date ASC")
     result = cursor.fetchall()
     stats = []
+
+    cursor2 = mysql.connection.cursor()
+    cursor2.execute("SELECT * FROM healthdash_measurements ORDER BY value ASC LIMIT 1")
+    result2 = cursor2.fetchone()
 
     first_item = result[0]
     last_item = (result[len(result) - 1])
     current_length = 173
     bmi = float(last_item[3]) / (current_length/100)**2
+    lowest_weight = result2[3]
 
     stats.append({
         'start_weight': first_item[3],
         'current_weight': last_item[3],
         'weight_loss': str(round((float(first_item[3]) - float(last_item[3])), 1)),
-        'current_bmi': str(round(bmi, 1))
+        'current_bmi': str(round(bmi, 1)),
+        'lowest_weight': lowest_weight,
+        'increase': str(round((float(last_item[3]) - float(lowest_weight)), 1)),
     })
 
     return jsonify(stats)
-
-@healthdash.route('/activities')
-@token_required
-def HealthDash_Activities():
-    mysql = current_app.config['MYSQL']
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT * FROM healthdash_activities")
-    row_headers = [x[0] for x in cursor.description]
-    result = cursor.fetchall()
-    json_data=[]
-    for measurement in result:
-        json_data.append(dict(zip(row_headers,measurement)))
-
-    return jsonify(json_data)
-
-@healthdash.route('/bmi')
-@token_required
-def HealthDash_BMI():
-    current_length = 173
-    mysql = current_app.config['MYSQL']
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT value FROM healthdash_measurements WHERE type='weight' ORDER BY healthdash_measurements.date DESC LIMIT 1")
-    result = cursor.fetchone()
-    bmi = float(result[0]) / (current_length/100)**2
-
-    return jsonify({"bmi" : str(round(bmi, 1))})
